8095295968b0ff2cc78b3c9b614e547f2ad65ef2
Jan <MapManagement@protonmail.com>
2025-02-18 23:57:27 +0100

    Finish book related modelling

--- backend/Models/BookRelated.cs
+++ backend/Models/BookRelated.cs
@@ -1,59 +1,48 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Bookx.Models
 {
     public class Author
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
-        public DateTime Birthdate { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public ICollection<Book> Books { get; } = new List<Book>();
     }
 
     public class Book
     {
-        [Key]
         public string Isbn { get; set; }
-        [Required]
         public string Title { get; set; }
         public string CoverPath { get; set; }
-        [Required]
         public int NumerOfPages { get; set; }
         public string ShopLink { get; set; }
         public string Blurb { get; set; }
-        [Required]
-        public Author Author { get; set; }
-        [Required]
-        public Language Language { get; set; }
-        [Required]
-        public Publisher Publisher { get; set; }
-        public List<Genre> Genres { get; set; }
+        public ICollection<Author> Authors { get; } = new List<Author>();
+        public int LanguageId { get; set; }
+        public Language Language { get; set; } = null!;
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; } = null!;
+        public ICollection<Genre> Genres { get; } = new List<Genre>();
     }
 
     public class Publisher
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
+        public string Name { get; }
+        public ICollection<Book> Books { get; } = new List<Book>();
     }
 
     public class Genre
     {
-        [Key]
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
+        public ICollection<Book> Books { get; } = new List<Book>();
     }
 
     public class Language
     {
-        [Key]
         public int id { get; set; }
-        [Required]
         public string Name { get; set; }
+        public ICollection<Book> Books { get; } = new List<Book>();
     }
 }
--- backend/Models/BookxContext.cs
+++ backend/Models/BookxContext.cs
@@ -1,10 +1,48 @@
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookx.Models
 {
     public class BookxContext : DbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Language> Languages { get; set; }
 
+        private readonly string _dbConnectionString;
+
+        public BookxContext()
+        {
+            _dbConnectionString = "";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("<connection string>");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(a => a.Books)
+                .WithMany(b => b.Authors);
+
+            modelBuilder.Entity<Genre>()
+                .HasMany(g => g.Books)
+                .WithMany(b => b.Genres);
+
+            modelBuilder.Entity<Publisher>()
+                .HasMany(p => p.Books)
+                .WithOne(b => b.Publisher)
+                .HasForeignKey(b => b.PublisherId)
+                .IsRequired();
+
+            modelBuilder.Entity<Language>()
+                .HasMany(l => l.Books)
+                .WithOne(b => b.Language)
+                .HasForeignKey(b => b.LanguageId)
+                .IsRequired();
+        }
     }
 }
