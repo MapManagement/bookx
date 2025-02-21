24a4b99483980352b2a59fb81aa967b61339716e
Jan <MapManagement@protonmail.com>
2025-02-21 17:39:50 +0100

    backend: Add static helper class for conversion from db to proto

---
+++ backend/Helpers/ProtoDbEntityConverter.cs
@@ -0,0 +1,76 @@
+using Bookx.Models;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Bookx.Helpers;
+
+public static class ProtoDbEntityConverter
+{
+    public static SingleAuthor DbToProtoAuthor(Author dbAuthor)
+    {
+        return new SingleAuthor()
+        {
+            Id = dbAuthor.Id,
+            FirstName = dbAuthor.FirstName,
+            LastName = dbAuthor.LastName,
+            Birthdate = Timestamp.FromDateTime(dbAuthor.Birthdate.Value)
+        };
+    }
+
+    public static SingleLanguage DbToProtoLanguage(Language dbLanguage)
+    {
+        return new SingleLanguage()
+        {
+            Id = dbLanguage.Id,
+            Name = dbLanguage.Name
+        };
+    }
+
+    public static SinglePublisher DbToProtoPublisher(Publisher dbPublisher)
+    {
+        return new SinglePublisher()
+        {
+            Id = dbPublisher.Id,
+            Name = dbPublisher.Name
+        };
+    }
+
+    public static SingleGenre DbToProtoGenre(Genre dbGenre)
+    {
+        return new SingleGenre()
+        {
+            Id = dbGenre.Id,
+            Name = dbGenre.Name
+        };
+    }
+
+    public static SingleBook DbToProtoBook(Book dbBook)
+    {
+        var protoLanguage = DbToProtoLanguage(dbBook.Language);
+        var protoPublisher = DbToProtoPublisher(dbBook.Publisher);
+
+
+        var protoBook = new SingleBook()
+        {
+            Isbn = dbBook.Isbn,
+            Title = dbBook.Title,
+            //CoverPath
+            NumberOfPages = dbBook.NumerOfPages,
+            ShopLink = dbBook.ShopLink,
+            Blurb = dbBook.Blurb,
+            Language = protoLanguage,
+            Publisher = protoPublisher,
+        };
+
+        foreach (var dbGenre in dbBook.Genres)
+        {
+            protoBook.Genres.Add(DbToProtoGenre(dbGenre));
+        }
+
+        foreach (var dbAuthor in dbBook.Authors)
+        {
+            protoBook.Authors.Add(DbToProtoAuthor(dbAuthor));
+        }
+
+        return protoBook;
+    }
+}
--- backend/Services/BookRelatedService.cs
+++ backend/Services/BookRelatedService.cs
@@ -1,18 +1,56 @@
+using Grpc.Core;
+using Bookx.Models;
+using Bookx.Helpers;
+
 namespace Bookx.Services;
 
 public class BookRelatedService : Books.BooksBase
 {
     private readonly ILogger<BookRelatedService> _logger;
+    private readonly BookxContext _bookxContext;
+
     public BookRelatedService(ILogger<BookRelatedService> logger)
     {
         _logger = logger;
     }
 
-    /*public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    public override Task<SingleAuthor> GetSingleAuthor(SingleAuthorRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new HelloReply
-        {
-            Message = "Hello " + request.Name
-        });
-    }*/
+        var dbAuthor = _bookxContext.Find<Author>(request.Id);
+        var protoAuthor = ProtoDbEntityConverter.DbToProtoAuthor(dbAuthor);
+
+        return Task.FromResult(protoAuthor);
+    }
+
+    public override Task<SingleGenre> GetSingleGenre(SingleGenreRequest request, ServerCallContext context)
+    {
+        var dbGenre = _bookxContext.Find<Genre>(request.Id);
+        var protoGenre = ProtoDbEntityConverter.DbToProtoGenre(dbGenre);
+
+        return Task.FromResult(protoGenre);
+    }
+
+    public override Task<SingleLanguage> GetSingleLanguage(SingleLanguageRequest request, ServerCallContext context)
+    {
+        var dbLanguage = _bookxContext.Find<Language>(request.Id);
+        var protoLanguage = ProtoDbEntityConverter.DbToProtoLanguage(dbLanguage);
+
+        return Task.FromResult(protoLanguage);
+    }
+
+    public override Task<SinglePublisher> GetSinglePublisher(SinglePublisherRequest request, ServerCallContext context)
+    {
+        var dbPublisher = _bookxContext.Find<Publisher>(request.Id);
+        var protoPublisher = ProtoDbEntityConverter.DbToProtoPublisher(dbPublisher);
+
+        return Task.FromResult(protoPublisher);
+    }
+
+    public override Task<SingleBook> GetSingleBook(SingleBookRequest request, ServerCallContext context)
+    {
+        var dbBook = _bookxContext.Find<Book>(request.Isbn);
+        var protoBook = ProtoDbEntityConverter.DbToProtoBook(dbBook);
+
+        return Task.FromResult(protoBook);
+    }
 }
