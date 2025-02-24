8942bbafea8d39654c64888933b67774906b86c4
Jan <MapManagement@protonmail.com>
2025-02-24 17:01:17 +0100

    backend: Add new migrations for DB

--- backend/Program.cs
+++ backend/Program.cs
@@ -31,6 +31,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
+app.MapGrpcService<AuthenticationService>();
 app.MapGrpcService<BookRelatedService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
