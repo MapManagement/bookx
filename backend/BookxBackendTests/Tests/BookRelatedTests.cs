3c7da75ac0b321d5621dc4439098a79436358d83
Jan <MapManagement@protonmail.com>
2025-04-15 00:33:09 +0200

    backend: Start tests for user related calls

--- backend/BookxBackendTests/Tests/BookRelatedTests.cs
+++ backend/BookxBackendTests/Tests/BookRelatedTests.cs
@@ -7,31 +7,6 @@ namespace BookxBackendTests.Tests
     {
         #region Tests
 
-        [Theory]
-        [InlineData("Herman", "Hesse", "Herman.Hesse@mail.com")]
-        [InlineData("Ernst Theodor Amadeus", "Hoffmann", "ETA.Hoffmann@mail.com")]
-        [InlineData("Friedrich", "Dürrenmatt", "Friedrich.Dürrenmatt@mail.com")]
-        public async Task SuccessfullyAddNewAuthor(string username, string password, string mailAddress)
-        {
-            using (var backend = GrpcCallHelper.CreateTestBackend())
-            {
-                var authClient = new Authenticator.AuthenticatorClient(backend.GrpcChannel);
-
-                var registerRequest = new RegisterRequest()
-                {
-                    Username = username,
-                    Password = password,
-                    MailAddress = mailAddress
-                };
-
-                var registerReply = await authClient.RegisterAsync(registerRequest);
-
-                Assert.NotNull(registerReply);
-                Assert.True(registerReply.ValidRegistration);
-                Assert.NotNull(registerReply.Token);
-            }
-        }
-
         #endregion
     }
 }
--- backend/BookxBackendTests/Tests/UserRelatedTest.cs
+++ backend/BookxBackendTests/Tests/UserRelatedTest.cs
@@ -1,10 +1,20 @@
-using BookxBackendTests.Fixtures;
 using BookxProtos;
+using BookxBackendTests.Fixtures;
+using BookxBackendTests.Helpers;
+using Grpc.Core;
 
 namespace BookxBackendTests.Tests
 {
     public class UserRelatedTests
     {
+        #region Fields
+
+        const string LoginUsername = "Book";
+        const string LoginPassword = "Reader";
+        const string LoginMailAddress = "book@reader.com";
+
+        #endregion
+
         #region Methods
 
         private BookxBackendTestFixture CreateTestBackend()
@@ -15,6 +25,38 @@ namespace BookxBackendTests.Tests
         #endregion
 
         #region Tests
+
+        [Theory]
+        [InlineData("9783641279110", 8, "Some comment", true)]
+        public async Task SuccessfulAddOwnedBook(string isbn, int rating, string comment, bool wouldRecommend)
+        {
+            using (var backend = GrpcCallHelper.CreateTestBackend())
+            {
+                var authClient = new Authenticator.AuthenticatorClient(backend.GrpcChannel);
+
+                var registerReply = await GrpcCallHelper.RegisterNewUser(authClient, LoginUsername, LoginPassword, LoginMailAddress);
+
+                Assert.True(registerReply.ValidRegistration);
+
+                var userClient = new UserService.UserServiceClient(backend.GrpcChannel);
+
+                var newBookRequest = new AddSingleOwnedBook()
+                {
+                    Isbn = isbn,
+                    Rating = rating,
+                    Comment = comment,
+                    WouldRecommend = wouldRecommend
+                };
+
+                var requestMetadata = new Metadata();
+                requestMetadata.Add("Authorization", $"Bearer {registerReply.Token}");
+
+                var newBookReply = await userClient.AddOwnedBookAsync(newBookRequest, requestMetadata);
+
+                Assert.True(newBookReply.Success);
+            }
+        }
+
 
         #endregion
     }
